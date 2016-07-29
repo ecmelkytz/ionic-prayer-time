7020c3014cebc8de1f3cee61082a9eceefd9ab4c
Ecmel Kaytazoğlu <ecmel.kytz@gmail.com>
2016-07-29 09:01:03 +0300

    set to select town

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,17 @@
-angular.module('starter').factory('Cities', function($http) {
+angular.module('starter')
+
+.factory('Cities', function($http) {
   var get = function(code) {
     return $http.get("http://www.diyanet.gov.tr/PrayerTime/FillState?countryCode=" + code);
+  };
+  return {
+    get: get
+  }
+})
+
+.factory('Towns', function($http) {
+  var get = function(city_id) {
+    return $http.get("http://www.diyanet.gov.tr/PrayerTime/FillCity?itemId=" + city_id);
   };
   return {
     get: get
