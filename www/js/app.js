7d1ba749ae21ac81383ac77467384c5812ae3cfd
Ecmel Kaytazoğlu <ecmel.kytz@gmail.com>
2016-08-10 14:32:52 +0300

    .

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'ionic-modal-select', 'circle.countdown'])
+angular.module('starter', ['ionic', 'ionic-modal-select'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
